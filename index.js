4d94ed681b55aa901b25a68666eb7cd22b737038
vijaynaga <vijay@triplepulse.in>
2016-07-30 19:49:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var http = require('http')
+var express = require('express')
+var stormpath = require('stormpath')
+
+var app = express()
+
+app.get('/notes', function(req, res) {
+  res.json({notes: "Your API call to this get function was successfull."})
+})
+
+app.listen(9000)
+
